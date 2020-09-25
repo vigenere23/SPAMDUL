247333d6618e62530e0ac9b6c6614151f94d61d4
sebastien <sebastien.nantel03@gmail.com>
2020-09-25 08:35:14 -0400

    It is now impossible (again) to create a weekend day as a day to access campus

--- src/main/java/ca/ulaval/glo4003/spamdul/entity/user/UserFactory.java
+++ src/main/java/ca/ulaval/glo4003/spamdul/entity/user/UserFactory.java
@@ -6,10 +6,9 @@ import java.time.LocalDate;
 public class UserFactory {
 
   public User create(String name, Gender gender, LocalDate birthDate, DayOfWeek dayToAccessCampus) {
-    //TODO Utile si le client valide que c<est bien le cas, ne pas oublier de decommenter les tests
-//    if (dayToAccessCampus == DayOfWeek.SATURDAY || dayToAccessCampus == DayOfWeek.SUNDAY) {
-//      throw new InvalidDayToAccessCampusException("The day to access the campus must be between Monday and Friday");
-//    }
+    if (dayToAccessCampus == DayOfWeek.SATURDAY || dayToAccessCampus == DayOfWeek.SUNDAY) {
+      throw new InvalidDayToAccessCampusException("The day to access the campus must be between Monday and Friday");
+    }
 
     return new User(new UserId(), name, gender, birthDate, dayToAccessCampus);
   }
