cc405e9adac19201697216f5933c3557c18b1c56
Léandre Gagnon-Lewis <leandre.gagnon-lewis.1@ulaval.ca>
2020-10-07 21:49:43 -0400

    ajouter calendar tests

--- src/main/java/ca/ulaval/glo4003/spamdul/infrastructure/calendar/HardCodedCalendar.java
+++ src/main/java/ca/ulaval/glo4003/spamdul/infrastructure/calendar/HardCodedCalendar.java
@@ -3,6 +3,7 @@ package ca.ulaval.glo4003.spamdul.infrastructure.calendar;
 import ca.ulaval.glo4003.spamdul.entity.timeperiod.Calendar;
 import ca.ulaval.glo4003.spamdul.entity.timeperiod.Semester;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class HardCodedCalendar implements Calendar {
@@ -10,13 +11,13 @@ public class HardCodedCalendar implements Calendar {
     public LocalDateTime getStartOfSemester(Semester semester) {
         switch (semester.getSeason()) {
             case 'A':
-                return LocalDateTime.of(semester.getYear(), 9, 1, 0, 0);
+                return LocalDate.of(semester.getYear(), 9, 1).atStartOfDay();
             case 'H':
-                return LocalDateTime.of(semester.getYear(), 1, 15, 0, 0);
+                return LocalDate.of(semester.getYear(), 1, 1).atStartOfDay();
             case 'E':
-                return LocalDateTime.of(semester.getYear(), 5, 7, 0, 0);
+                return LocalDate.of(semester.getYear(), 5, 1).atStartOfDay();
             default:
-                throw new RuntimeException();
+                throw new RuntimeException("bad season");
         }
     }
 
@@ -24,13 +25,13 @@ public class HardCodedCalendar implements Calendar {
     public LocalDateTime getEndOfSemester(Semester semester) {
         switch (semester.getSeason()) {
             case 'A':
-                return LocalDateTime.of(semester.getYear(), 12, 23, 0, 0);
+                return LocalDateTime.of(semester.getYear(), 12, 31, 23, 59,59);
             case 'H':
-                return LocalDateTime.of(semester.getYear(), 5, 5, 0, 0);
+                return LocalDateTime.of(semester.getYear(), 4, 30, 23, 59,59);
             case 'E':
-                return LocalDateTime.of(semester.getYear(), 8, 15, 0, 0);
+                return LocalDateTime.of(semester.getYear(), 8, 31, 23, 59,59);
             default:
-                throw new RuntimeException();
+                throw new RuntimeException("bad season");
         }
     }
 
---
+++ src/test/java/ca/ulaval/glo4003/spamdul/infrastructure/calendar/HardCodedCalendarTest.java
@@ -0,0 +1,68 @@
+package ca.ulaval.glo4003.spamdul.infrastructure.calendar;
+
+import ca.ulaval.glo4003.spamdul.entity.timeperiod.Semester;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class HardCodedCalendarTest {
+    private final int A_YEAR = 2098;
+
+    private final HardCodedCalendar hardCodedCalendar = new HardCodedCalendar();
+
+    @Test
+    public void givenAutumnSemester_whenGettingStartDate_shouldReturn1SeptOfSameYear() {
+        Semester semester = new Semester('A', A_YEAR);
+
+        LocalDateTime result = hardCodedCalendar.getStartOfSemester(semester);
+
+        assertThat(result).isEqualTo(LocalDateTime.of(A_YEAR, 9, 1, 0, 0));
+    }
+
+    @Test
+    public void givenWinterSemester_whenGettingStartDate_shouldReturn1JanOfSameYear() {
+        Semester semester = new Semester('H', A_YEAR);
+
+        LocalDateTime result = hardCodedCalendar.getStartOfSemester(semester);
+
+        assertThat(result).isEqualTo(LocalDateTime.of(A_YEAR, 1, 1, 0, 0));
+    }
+
+    @Test
+    public void givenSummerSemester_whenGettingStartDate_shouldReturn1MayOfSameYear() {
+        Semester semester = new Semester('E', A_YEAR);
+
+        LocalDateTime result = hardCodedCalendar.getStartOfSemester(semester);
+
+        assertThat(result).isEqualTo(LocalDateTime.of(A_YEAR, 5, 1, 0, 0));
+    }
+
+    @Test
+    public void givenAutumnSemester_whenGettingEndDate_shouldReturn31DecOfSameYear() {
+        Semester semester = new Semester('A', A_YEAR);
+
+        LocalDateTime result = hardCodedCalendar.getEndOfSemester(semester);
+
+        assertThat(result).isEqualTo(LocalDateTime.of(A_YEAR, 12, 31, 23, 59,59));
+    }
+
+    @Test
+    public void givenWinterSemester_whenGettingEndDate_shouldReturn30AprilOfSameYear() {
+        Semester semester = new Semester('H', A_YEAR);
+
+        LocalDateTime result = hardCodedCalendar.getEndOfSemester(semester);
+
+        assertThat(result).isEqualTo(LocalDateTime.of(A_YEAR, 4, 30, 23, 59,59));
+    }
+
+    @Test
+    public void givenSummerSemester_whenGettingEndDate_shouldReturn31AugOfSameYear() {
+        Semester semester = new Semester('E', A_YEAR);
+
+        LocalDateTime result = hardCodedCalendar.getEndOfSemester(semester);
+
+        assertThat(result).isEqualTo(LocalDateTime.of(A_YEAR, 8, 31, 23, 59,59));
+    }
+}
