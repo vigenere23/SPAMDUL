797c4e4f6386eec4cd57673150ef4d86ccdc1f4e
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2020-10-16 12:51:57 -0400

    changed to PUT

--- src/main/java/ca/ulaval/glo4003/spamdul/infrastructure/ui/carboncredits/CarbonCreditsResource.java
+++ src/main/java/ca/ulaval/glo4003/spamdul/infrastructure/ui/carboncredits/CarbonCreditsResource.java
@@ -5,6 +5,7 @@ import ca.ulaval.glo4003.spamdul.infrastructure.ui.carboncredits.dto.CarbonCredi
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,7 +13,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/carbon-credits")
 public interface CarbonCreditsResource {
 
-  @POST
+  @PUT
   @Path("/toggle")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
