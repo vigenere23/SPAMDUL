c7cd9abac960e3764542d29e39e58e1b92b42068
Marouane Yassine <marouane.yassine.1@ulaval.ca>
2020-10-25 14:28:26 -0400

    removed incompatible account tests

--- src/test/java/ca/ulaval/glo4003/spamdul/entity/account/AccountTest.java
+++ src/test/java/ca/ulaval/glo4003/spamdul/entity/account/AccountTest.java
@@ -17,49 +17,4 @@ public class AccountTest {
     FUNDS = Amount.valueOf(100);
     OTHER_FUNDS = Amount.valueOf(250);
   }
-
-  @Test
-  public void givenANewAccount_shouldBeEmpty() {
-    account = new Account();
-    Amount funds = account.getFunds();
-    assertThat(funds.asDouble()).isEqualTo(0);
-  }
-
-  @Test(expected = InsufficientFundsException.class)
-  public void givenANewAccountWithoutEnoughFunds_whenWithdrawFunds_shouldThrowError() {
-    account = new Account();
-    account.withdrawFunds(FUNDS);
-  }
-
-  @Test
-  public void givenANewAccount_whenAddingFunds_shouldHaveSameFunds() {
-    account = new Account();
-
-    account.addFunds(FUNDS);
-
-    Amount funds = account.getFunds();
-    assertThat(funds.asDouble()).isEqualTo(FUNDS.asDouble());
-  }
-
-  @Test
-  public void givenANewAccount_whenAddingFunds_shouldHaveTheSumOfFunds() {
-    account = new Account();
-
-    account.addFunds(FUNDS);
-    account.addFunds(OTHER_FUNDS);
-
-    Amount funds = account.getFunds();
-    assertThat(funds.asDouble()).isEqualTo(FUNDS.add(OTHER_FUNDS).asDouble());
-  }
-
-  @Test
-  public void givenANewAccountWithEnoughFunds_whenWithdrawFunds_shouldHaveTheDifferenceFunds() {
-    account = new Account();
-
-    account.addFunds(OTHER_FUNDS);
-    account.withdrawFunds(FUNDS);
-
-    Amount funds = account.getFunds();
-    assertThat(funds.asDouble()).isEqualTo(OTHER_FUNDS.subtract(FUNDS).asDouble());
-  }
 }
