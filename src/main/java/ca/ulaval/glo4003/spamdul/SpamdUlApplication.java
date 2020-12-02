e24540baaccb308a515289e7421a901c9124d05e
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2020-12-02 08:41:22 -0500

    created app class

---
+++ src/main/java/ca/ulaval/glo4003/spamdul/SpamdUlApplication.java
@@ -0,0 +1,6 @@
+package ca.ulaval.glo4003.spamdul;
+
+public interface SpamdUlApplication {
+
+  void start();
+}
--- src/main/java/ca/ulaval/glo4003/spamdul/SpamdUlMain.java
+++ src/main/java/ca/ulaval/glo4003/spamdul/SpamdUlMain.java
@@ -1,56 +1,10 @@
 package ca.ulaval.glo4003.spamdul;
 
-import ca.ulaval.glo4003.spamdul.context.main.ContextType;
-import ca.ulaval.glo4003.spamdul.context.main.MainContext;
-import ca.ulaval.glo4003.spamdul.context.main.MainContextFactory;
-import ca.ulaval.glo4003.spamdul.infrastructure.http.CORSResponseFilter;
-import java.util.HashSet;
-import java.util.Set;
-import javax.ws.rs.core.Application;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
-
 @SuppressWarnings("all")
 public class SpamdUlMain {
 
   public static void main(String[] args) throws Exception {
-    ContextType contextType = ContextType.parse(System.getenv("SPAMDUL_API_MODE"));
-    MainContext mainContext = new MainContextFactory().create(contextType);
-
-    ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    contextHandler.setContextPath("/api/");
-    ResourceConfig resourceConfig = ResourceConfig.forApplication(new Application() {
-      @Override
-      public Set<Object> getSingletons() {
-        Set<Object> resources = new HashSet<>();
-        mainContext.registerResources(resources);
-        return resources;
-      }
-    });
-    resourceConfig.register(CORSResponseFilter.class);
-
-    ServletContainer servletContainer = new ServletContainer(resourceConfig);
-    ServletHolder servletHolder = new ServletHolder(servletContainer);
-    contextHandler.addServlet(servletHolder, "/*");
-
-    ContextHandlerCollection contexts = new ContextHandlerCollection();
-    contexts.setHandlers(new Handler[]{contextHandler});
-    Server server = new Server(8080);
-    server.setHandler(contexts);
-
-    try {
-      server.start();
-      server.join();
-    } catch (Exception exception) {
-      exception.printStackTrace();
-    } finally {
-      server.destroy();
-      mainContext.destroy();
-    }
+    SpamdUlApplication app = new JettyJerseyApp();
+    app.start();
   }
 }
